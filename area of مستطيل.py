645e9693cf642110b6bad558de00bf4715aec60e
abdogamal2 <abdogamalmohamedjshbshhs@gmail.com>
2026-01-16 00:37:13 +0200

    area of rectangular

---
+++ area of مستطيل.py
@@ -0,0 +1,10 @@
+
+length=float (input ("enter your length: "))
+wideth=float (input ("enter your wideth: "))
+area=length * wideth
+cost=float(input("enter the cost of 1m² : "))
+y=cost * area 
+round(y,0)
+print (f"cost = {y} $")
+print (f"the area is : {round (area,3)}cm²")
+
