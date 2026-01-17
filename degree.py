2e9e5233bfd087aad9e766413499b60e562b55b5
abdogamal2 <abdogamalmohamedjshbshhs@gmail.com>
2026-01-17 14:48:48 +0200

    anthor if

---
+++ degree.py
@@ -0,0 +1,12 @@
+degree=float(input ("enter your degree : "))
+if 100>=degree>=90:
+    print ("A")    
+elif 90>degree>=75:
+    print ("B")
+elif 75>degree>=50:
+    print ("D") 
+elif 0<=degree<50:
+    print ("F") 
+else :
+    print ("invelved input")
+
