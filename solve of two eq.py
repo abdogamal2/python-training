de16921c3ce511426d153414fde726930376efd1
abdogamal2 <abdogamalmohamedjshbshhs@gmail.com>
2026-01-29 16:32:11 +0200

    solve of two eq

---
+++ solve of two eq.py
@@ -0,0 +1,17 @@
+print ("welcom to my app ")
+print ("""
+          ***first eq_1***\n""")
+a=float(input ("enter conctant of x : "))
+b=float(input ("enter conctant of y : "))
+c=float(input ("enter conctant : "))
+print ("""
+          ***sacend eq_2***\n""")
+d=float(input ("enter conctant of x : "))
+f=float(input ("enter conctant of y : "))
+e=float(input ("enter conctant : "))
+y=(e-((d*c)/a))/(((-d*b)/a)+f)
+x=(((-b*y)+c)/a)
+print (f"y = {y}")
+print (f"x = {x}")
+print (f"[({x},{y})]")
+
